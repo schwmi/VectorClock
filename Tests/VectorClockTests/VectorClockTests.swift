122cbb8448308bc48d4debc71adf28d3188a2480
Michael Schwarz <michael_schwarz@me.com>
2020-07-26 16:34:02 +0200

    Test merging

--- Tests/VectorClockTests/VectorClockTests.swift
+++ Tests/VectorClockTests/VectorClockTests.swift
@@ -28,9 +28,26 @@ final class VectorClockTests: XCTestCase {
         let incrementedABBA = incrementedABB.incrementing("A")
         XCTAssertEqual(incrementedABBA.description, "<A=2, B=2>")
     }
+    
+    func testMerge() {
+        let clock = VectorClock<String>()
+        
+        // Increment actor A
+        let incrementedA = clock.incrementing("A")
+        XCTAssertEqual(incrementedA.description, "<A=1>")
+        
+        // Increment actor B
+        let incrementedB = clock.incrementing("B")
+        XCTAssertEqual(incrementedB.description, "<B=1>")
+        
+        // Merge A with B
+        let mergedAB = incrementedB.merging(incrementedA)
+        XCTAssertEqual(mergedAB.description, "<A=1, B=1>")
+    }
 
     static var allTests = [
         ("testIncrement", testIncrement),
+        ("testMerge", testMerge),
         ("testNewEmptyClock", testNewEmptyClock)
     ]
 }
