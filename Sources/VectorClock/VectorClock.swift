3310b72663baded9a791a068d0c35f26b5f275a1
Michael Schwarz <michael_schwarz@me.com>
2020-07-26 17:31:14 +0200

    Add logic for equality check

--- Sources/VectorClock/VectorClock.swift
+++ Sources/VectorClock/VectorClock.swift
@@ -1,4 +1,4 @@
-
+/// A Vector clock which ensures a total order by additionally adding a timestamp
 public struct VectorClock<ActorID: Equatable & Hashable> {
     
     private var clocksByActors: [ActorID: Int]
@@ -24,6 +24,56 @@ public struct VectorClock<ActorID: Equatable & Hashable> {
     func merging(_ clock: VectorClock) -> VectorClock {
         let mergedClocks = self.clocksByActors.merging(clock.clocksByActors) { max($0, $1) }
         return VectorClock(clocksByActors: mergedClocks)
+    }
+}
+
+// MARK: - Comparable
+
+extension VectorClock: Comparable {
+
+    public static func < (lhs: VectorClock<ActorID>, rhs: VectorClock<ActorID>) -> Bool {
+        var isEqual = true
+        for (key, value) in lhs.clocksByActors {
+            let rhsValue = rhs.clocksByActors[key, default: 0]
+            if value <= rhsValue {
+                if isEqual && value < rhsValue {
+                    isEqual = false
+                }
+            } else {
+                return false
+            }
+        }
+        if isEqual == true && lhs.clocksByActors.count == rhs.clocksByActors.count {
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    public static func == (lhs: VectorClock<ActorID>, rhs: VectorClock<ActorID>) -> Bool {
+        guard lhs.clocksByActors.count > 0 || rhs.clocksByActors.count > 0 else { return true }
+        
+        var lhsHasGreaterComponent = false
+        var rhsHasGreaterCompnent = false
+        var onlyEqualComponents = true
+        for (key, value) in lhs.clocksByActors {
+            let otherValue = rhs.clocksByActors[key, default: 0]
+            if value > otherValue {
+                lhsHasGreaterComponent = true
+                onlyEqualComponents = false
+                break
+            }
+        }
+        
+        for (key, value) in rhs.clocksByActors {
+            let otherValue = lhs.clocksByActors[key, default: 0]
+            if value > otherValue {
+                rhsHasGreaterCompnent = true
+                onlyEqualComponents = false
+                break
+            }
+        }
+        return (lhsHasGreaterComponent && rhsHasGreaterCompnent) || onlyEqualComponents
     }
 }
 
--- Tests/VectorClockTests/VectorClockTests.swift
+++ Tests/VectorClockTests/VectorClockTests.swift
@@ -44,6 +44,23 @@ final class VectorClockTests: XCTestCase {
         let mergedAB = incrementedB.merging(incrementedA)
         XCTAssertEqual(mergedAB.description, "<A=1, B=1>")
     }
+    
+    func testComparison() {
+        // Test empty clock comparison
+        let clock1 = VectorClock<String>()
+        let clock2 = VectorClock<String>()
+      //  XCTAssertEqual(clock1, clock2)
+        
+        // Increment actor A
+        let clock1A = clock1.incrementing("A")
+       // XCTAssertTrue(clock1 < clock1A)
+       // XCTAssertTrue(clock1A == clock1A)
+        
+        // Increment actor B
+        let clock2B = clock2.incrementing("B")
+        XCTAssertTrue(clock1A == clock2B)
+    
+    }
 
     static var allTests = [
         ("testIncrement", testIncrement),
